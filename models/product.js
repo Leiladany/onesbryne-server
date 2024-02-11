329e98b44845b56043217d8d747ca4fc3ae1e970
thetiagogil <talmeidagil@gmail.com>
2024-02-11 20:55:37 +0000

    added img tag to product model

--- models/product.js
+++ models/product.js
@@ -2,14 +2,19 @@ const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
   name: { type: String, required: true },
+  img: { type: String, required: true },
   size: {
     type: String,
     required: true,
     enum: ["XXS", "XS", "S", "M", "L", "XL", "XXL"],
   },
   price: { type: Number, required: true },
-  description: { type: String },
-  type: { type: String, required: true, enum: ["available", "sold"] },
+  description: { type: String, required: true },
+  type: {
+    type: String,
+    required: true,
+    enum: ["available", "not available", "sold"],
+  },
 });
 
 const Product = mongoose.model("Product", productSchema);
