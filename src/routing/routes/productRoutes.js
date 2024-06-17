1073f4fd46e0f1c185640cda4555e87b997333da
thetiagogil <talmeidagil@hotmail.com>
2024-06-17 18:01:31 +0100

    disable admin routes

--- src/routing/routes/productRoutes.js
+++ src/routing/routes/productRoutes.js
@@ -1,15 +1,15 @@
 const ProductController = require("../controllers/productController");
 const express = require("express");
 const router = express.Router();
-const { isAdmin } = require("../../middlewares/authentication");
+/* const { isAdmin } = require("../../middlewares/authentication"); */
 
 router.get("/", ProductController.getAllProducts);
 router.get("/status/available", ProductController.getAllAvailableProducts);
 router.get("/status/archived", ProductController.getAllArchivedProducts);
 router.get("/status/sold", ProductController.getAllSoldProducts);
-router.post("/", isAdmin, ProductController.createProduct);
+router.post("/", /* isAdmin, */ ProductController.createProduct);
 router.get("/:productId", ProductController.getProductById);
-router.put("/:productId", isAdmin, ProductController.updateProductById);
-router.delete("/:productId", isAdmin, ProductController.deleteProductById);
+router.put("/:productId", /* isAdmin, */ ProductController.updateProductById);
+router.delete("/:productId", /* isAdmin, */ ProductController.deleteProductById);
 
 module.exports = router;
